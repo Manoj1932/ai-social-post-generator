0c41d882ac1dc64b99ba3c05149e2cd181b58d37
Manoj <manoj@gmail.com>
2026-03-09 12:15:10 +0530

    fix frontend api url

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ const tone = document.getElementById("tone").value
 document.getElementById("loading").style.display="block"
 document.getElementById("result").innerText=""
 
-const response = await fetch("http://127.0.0.1:8000/generate",{
+const response = await fetch("https://ai-social-post-generator-n2ie.onrender.com/generate",{
 
 method:"POST",
 
