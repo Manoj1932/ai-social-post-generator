270a2afb280de038c363bf8b42631fd34e4cd78a
Manoj <manoj@gmail.com>
2026-03-09 09:30:49 +0530

    first commit

---
+++ frontend/script.js
@@ -0,0 +1,46 @@
+async function generate(){
+
+const platform = document.getElementById("platform").value
+const topic = document.getElementById("topic").value
+const tone = document.getElementById("tone").value
+
+document.getElementById("loading").style.display="block"
+document.getElementById("result").innerText=""
+
+const response = await fetch("http://127.0.0.1:8000/generate",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body: JSON.stringify({
+platform:platform,
+topic:topic,
+tone:tone
+})
+
+})
+
+const data = await response.json()
+
+document.getElementById("loading").style.display="none"
+document.getElementById("result").innerText=data.content
+
+}
+
+function copyText(){
+
+const text=document.getElementById("result").innerText
+
+navigator.clipboard.writeText(text)
+
+const btn=document.querySelector(".copy-btn")
+btn.innerText="✅ Copied!"
+
+setTimeout(()=>{
+btn.innerText="📋 Copy"
+},2000)
+
+}
